11d8d43aafd16dabdbae98518b815dbdf745bf19
Guy Osborn <guywilliamosborn@gmail.com>
2022-11-07 13:30:11 -0800

    completed mc

--- notebooks/project_functions3.py
+++ notebooks/project_functions3.py
@@ -1,98 +1,35 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 7,
-   "id": "7659c7b4-d051-4744-bc83-670443bf6529",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import pandas as pd\n",
-    "def load_and_process(rawData1,rawData2):\n",
-    "    \n",
-    "    df1 = pd.read_csv(rawData1,encoding = 'latin-1'),\n",
-    "    df2 = pd.read_csv(rawData2,encoding = 'latin-1'),\n",
-    "    \n",
-    "# removing unused variables \n",
-    "\n",
-    "    df1 = df1.drop(['rgdpe','country','currency_unit','i_cig','i_xm','i_xr','i_outlier','i_irr','ccon','cda','cgdpe','cgdpo','cn','ck','ctfp','rconna','rdana','pl_con','pl_da','pl_gdpo','cor_exp','statcap','csh_c','csh_i','csh_g','csh_x','csh_m','csh_r','pl_c','pl_i','pl_g','pl_x','pl_m','pl_n','pl_k','cwtfp','rgdpna','rnna','rkna','rtfpna','rwtfpna','labsh','irr','delta','xr'],axis=1),\n",
-    "    df2 = df2.drop(['Entity'], axis = 1),\n",
-    "    \n",
-    "# renaming variables for merge   \n",
-    "\n",
-    "    df2.rename(columns={  \n",
-    "                        'Code' :'countrycode',\n",
-    "                        'Year' : 'year',\n",
-    "                        'Days of vacation and holidays for full-time production workers in non-agricultural activities (Huberman & Minns 2007)' : 'Annual Days of Vacation'\n",
-    "                       }),\n",
-    "    df1 = df1.rename(columns={  \n",
-    "                        'rgdpo': 'Output GDP (USD millions)', \n",
-    "                        'pop' : 'Population (millions)',\n",
-    "                        'emp' : 'Number of Employees (millions)',\n",
-    "                        'avh' : 'Average annual hours worked', \n",
-    "                        'hc' : 'Human capital index',\n",
-    "                       }),\n",
-    "    \n",
-    "# merge \n",
-    "\n",
-    "    cleanedData = pd.merge(\n",
-    "        df1,\n",
-    "        df2,\n",
-    "        how = 'left',\n",
-    "        on = [\"countrycode\", \"year\"]\n",
-    "    ),\n",
-    "\n",
-    "# selecting countries\n",
-    "    \n",
-    "    cleanedData_with_index = cleanedData_with_index.set_index(\"countrycode\"),\n",
-    "    cleanedData = cleanedData_with_index.drop(['ABW','AGO','AIA','ALB','ARE','ARG','ARM','ATG',\n",
-    "       'AUT','AZE','BDI','BEN','BFA','BGD','BGR','BHR',\n",
-    "       'BHS','BIH','BLR','BLZ','BMU','BOL','BRA','BRB','BRN',\n",
-    "       'BTN','BWA', 'CAF', 'CHE', 'CHL', 'CHN', 'CIV','CMR',\n",
-    "       'COD', 'COG', 'COL', 'COM', 'CPV', 'CRI', 'CUW', 'CYM','CYP',\n",
-    "       'CZE', 'DJI', 'DMA','DOM', 'DZA', 'ECU', 'EGY','EST', 'ETH', 'FIN', 'FJI', 'GAB', 'GEO',\n",
-    "       'GHA', 'GIN', 'GMB', 'GNB', 'GNQ', 'GRC', 'GRD', 'GTM','GUY',\n",
-    "       'HKG', 'HND', 'HRV', 'HTI','HUN','IDN','IND','IRN',\n",
-    "       'IRQ', 'ISL', 'ISR', 'JAM', 'JOR', 'JPN', 'KAZ', 'KEN',\n",
-    "       'KGZ', 'KHM', 'KNA', 'KOR', 'KWT', 'LAO', 'LBN', 'LBR', 'LCA',\n",
-    "       'LKA', 'LSO', 'LTU', 'LUX', 'LVA', 'MAC', 'MAR', 'MDA', 'MDG',\n",
-    "       'MDV', 'MEX', 'MKD', 'MLI', 'MLT', 'MMR', 'MNE', 'MNG', 'MOZ',\n",
-    "       'MRT', 'MSR', 'MUS', 'MWI', 'MYS', 'NAM', 'NER', 'NGA', 'NIC','NOR', 'NPL', 'NZL', 'OMN', 'PAK', 'PAN', 'PER', 'PHL',\n",
-    "       'POL', 'PRT', 'PRY', 'PSE', 'QAT', 'ROU', 'RUS', 'RWA', 'SAU',\n",
-    "       'SDN', 'SEN', 'SGP', 'SLE', 'SLV', 'SRB', 'STP', 'SUR', 'SVK',\n",
-    "       'SVN', 'SWZ', 'SXM', 'SYC', 'SYR', 'TCA', 'TCD', 'TGO',\n",
-    "       'THA', 'TJK', 'TKM', 'TTO', 'TUN', 'TUR', 'TWN', 'TZA', 'UGA',\n",
-    "       'UKR', 'URY', 'UZB', 'VCT', 'VEN', 'VGB', 'VNM', 'YEM',\n",
-    "       'ZAF', 'ZMB', 'ZWE',], axis = 0)\n",
-    "    cleanedData = cleanedData.reset_index(\"countrycode\"),\n",
-    "    \n",
-    "# adding labour productivity   \n",
-    "    \n",
-    "    cleanedData['Labour Productivity'] = cleanedData['Output GDP (USD millions)'] / (cleanedData['Average annual hours worked'] * cleanedData['Number of Employees (millions)'])\n",
-    "    return d_cleaned\n",
-    "    "
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.12"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import pandas as pd
+def load_and_process(rawData1, rawData2):
+        df1 = pd.read_csv(rawData1,encoding = 'latin-1')
+        df2 = pd.read_csv(rawData2,encoding = 'latin-1')
+        df1 = df1.drop(['rgdpe',
+                        'country','currency_unit','i_cig','i_xm','i_xr',
+                        'i_outlier','i_irr','ccon','cda','cgdpe',
+                        'cgdpo','cn','ck','ctfp','rconna','rdana','pl_con','pl_da','pl_gdpo','cor_exp','statcap','csh_c',
+                        'csh_i','csh_g','csh_x','csh_m','csh_r','pl_c','pl_i','pl_g','pl_x',
+                        'pl_m','pl_n','pl_k','cwtfp','rgdpna','rnna','rkna','rtfpna','rwtfpna','labsh','irr','delta','xr'],axis=1)
+        df1 = df1.rename(columns={'rgdpo': 'Output GDP (USD millions)', 'pop' : 'Population (millions)','emp' : 'Number of Employees (millions)',
+                                  'avh' : 'Average annual hours worked',
+                                  'hc' : 'Human capital index'})
+        df2 = df2.drop(['Entity'], axis = 1)
+        df2 = df2.rename(columns={'Code':'countrycode','Year' : 'year',
+                                  'Days of vacation and holidays for full-time production workers in non-agricultural activities (Huberman & Minns 2007)' : 'Annual Days of Vacation'})
+        cleanedData = pd.merge(df1,df2,how = 'left',on = ["countrycode", "year"])
+        cleanedData_with_index = cleanedData.set_index("countrycode")
+        cleanedData = cleanedData_with_index.drop(['ABW','AGO','AIA','ALB','ARE','ARG','ARM','ATG','AUT','AZE','BDI','BEN','BFA','BGD','BGR','BHR','BHS','BIH',
+                                                   'BLR','BLZ','BMU','BOL','BRA','BRB','BRN','BTN','BWA', 'CAF', 'CHE', 'CHL', 'CHN', 'CIV','CMR',
+                                                   'COD', 'COG', 'COL', 'COM', 'CPV', 'CRI', 'CUW', 'CYM','CYP','CZE', 'DJI', 'DMA','DOM', 'DZA', 'ECU', 'EGY',
+                                                   'EST', 'ETH', 'FIN', 'FJI', 'GAB', 'GEO','GHA', 
+                                                   'GIN', 'GMB', 'GNB', 'GNQ', 'GRC', 'GRD', 'GTM','GUY','HKG', 'HND', 'HRV', 'HTI',
+                                                   'HUN','IDN','IND','IRN','IRQ', 'ISL', 'ISR', 'JAM', 'JOR', 'JPN', 'KAZ', 'KEN',
+                                                   'KGZ', 'KHM', 'KNA', 'KOR', 'KWT', 'LAO', 'LBN', 'LBR', 'LCA','LKA',
+                                                   'LSO', 'LTU', 'LUX', 'LVA', 'MAC', 'MAR', 'MDA', 'MDG',
+                                                   'MDV', 'MEX', 'MKD', 'MLI', 'MLT', 'MMR', 'MNE', 'MNG', 'MOZ','MRT', 
+                                                   'MSR', 'MUS', 'MWI', 'MYS', 'NAM', 'NER', 'NGA', 'NIC','NOR', 'NPL', 'NZL', 'OMN', 'PAK', 'PAN', 
+                                                   'PER', 'PHL','POL', 'PRT', 'PRY', 'PSE', 'QAT', 'ROU', 'RUS', 'RWA', 'SAU','SDN', 
+                                                   'SEN', 'SGP', 'SLE', 'SLV', 'SRB', 'STP', 'SUR', 'SVK','SVN', 'SWZ', 'SXM', 'SYC', 'SYR', 
+                                                   'TCA', 'TCD', 'TGO','THA', 'TJK', 'TKM', 'TTO', 'TUN', 'TUR', 'TWN', 'TZA', 'UGA','UKR', 'URY', 'UZB', 'VCT', 'VEN', 
+                                                   'VGB', 'VNM', 'YEM','ZAF', 'ZMB', 'ZWE',], axis = 0)
+        cleanedData = cleanedData.reset_index("countrycode")
+        cleanedData['Labour Productivity'] = cleanedData['Output GDP (USD millions)'] / (cleanedData['Average annual hours worked'] * cleanedData['Number of Employees (millions)'])
+        return cleanedData
